730400f03e32fc966fbd11e509b978ef2b4fd221
aliM494 <88322172+aliM494@users.noreply.github.com>
2021-10-17 15:23:55 +0330

    Added Stop & start button

--- src/Timer.js
+++ src/Timer.js
@@ -4,32 +4,38 @@ import './styles.css';
 
 var Interval;
 
-class Timer extends React.Component{
-    constructor(){
-      super();
-      this.state={
-        time:5,
-      }
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      time: 5,
     }
-  componentDidMount(){
-    Interval = setInterval(()=>{
-      this.setState({time:this.state.time-1});
-    },1000);
   }
-
-  componentDidUpdate(){
-    if(this.state.time==0){
+  start = () => {
+    Interval = setInterval(() => {
+      this.setState({ time: this.state.time - 1 });
+    }, 1000);
+  }
+  stop = () => {
+    clearInterval(Interval);
+  }
+  componentDidUpdate() {
+    if (this.state.time == 0) {
       clearInterval(Interval);
     }
   }
-    render(){
-      
-      return(
-        <h2 className='timer'>
-           {this.state.time}
-        </h2>
-      )
-    }
-  }
+  render() {
 
-  export default Timer;
+    return (
+      <>
+        <h2 className='timer'>
+          {this.state.time}
+        </h2>
+        <button className='button stop' onClick={this.stop}>Stop</button>
+        <button className='button start' onClick={this.start}>Start</button>
+      </>
+    )
+  }
+}
+
+export default Timer;
