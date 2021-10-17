4ffd807f1d3a11d75efb2e89e376c280f61bb636
aliM494 <88322172+aliM494@users.noreply.github.com>
2021-10-17 16:20:50 +0330

    Create timer

--- src/Timer.js
+++ src/Timer.js
@@ -8,31 +8,64 @@ class Timer extends React.Component {
   constructor() {
     super();
     this.state = {
-      time: 5,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      IsStart: false
     }
   }
   start = () => {
-    Interval = setInterval(() => {
-      this.setState({ time: this.state.time - 1 });
-    }, 1000);
-  }
-  stop = () => {
-    clearInterval(Interval);
-  }
-  componentDidUpdate() {
-    if (this.state.time == 0) {
-      clearInterval(Interval);
+    if (!this.state.IsStart) {
+      this.setState({
+        IsStart: true
+      });
+      Interval = setInterval(() => {
+        this.setState({
+          second: this.state.second + 1
+        });
+        if (this.state.second == 60) {
+          this.setState({
+            second: 0,
+            minute: this.state.minute + 1
+          });
+        }
+        if (this.state.minute == 60) {
+          this.setState({
+            minute: 0,
+            hour: this.state.hour + 1
+          });
+        }
+      }, 1000);
     }
   }
-  render() {
 
+  stop = () => {
+    this.setState({
+      IsStart: false
+    });
+    clearInterval(Interval);
+  }
+
+  reset = () => {
+    this.stop();
+    this.setState({
+      second: 0,
+      minute: 0,
+      hour: 0
+    });
+  }
+  render() {
+    let h = this.state.hour;
+    let m = this.state.minute;
+    let s = this.state.second;
     return (
       <>
         <h2 className='timer'>
-          {this.state.time}
+          {`${h > 9 ? h : "0" + h} : ${m > 9 ? m : "0" + m} : ${s > 9 ? s : "0" + s}`}
         </h2>
-        <button className='button stop' onClick={this.stop}>Stop</button>
-        <button className='button start' onClick={this.start}>Start</button>
+        <span className='button_box action_button reset' onClick={this.reset}>reset</span>
+        <span className='button_box action_button stop' onClick={this.stop}>stop</span>
+        <span className='button_box action_button start' onClick={this.start}>start</span>
       </>
     )
   }
--- src/Title.js
+++ src/Title.js
@@ -6,7 +6,7 @@ import './styles.css';
 class Title extends React.Component{
     render(){
       return(
-        <div>
+        <div className='main'>
         <h1 className='title'>سلام دوستان من</h1>
         <Timer/>
         </div>
