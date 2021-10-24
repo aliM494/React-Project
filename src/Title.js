c725a5c3d039f5067ea5320a978e119d1bc3378c
aliM494 <88322172+aliM494@users.noreply.github.com>
2021-10-24 17:18:10 +0330

    Added Time componnet to Title.js

--- src/Title.js
+++ src/Title.js
@@ -2,12 +2,14 @@ import React, { useState ,useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Timer from './Timer';
 import './styles.css';
+import TimeList from './Time';
 
 
 
 const Title = () => {
   const [title,setTitle] = useState("سلام دوستان من");
   const [isLight,setIsLight] = useState(true);
+  const [timearr,setTimearr]=useState(["00 : 00 : 05","00 : 06 : 56"]);
 
   useEffect(() => {
     console.log("useEffect")
@@ -20,6 +22,9 @@ const Title = () => {
     <div className='main' style={{background:(isLight) ? "white" : "black"}}>
       <h1 className='title'>{title}</h1>
       <Timer isLight={isLight} handleIsLight={handleIsLight} />
+      <TimeList>
+        {timearr}
+      </TimeList>
     </div>
   )
 }
