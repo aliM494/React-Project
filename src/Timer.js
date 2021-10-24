96a8c6d581e2a2bad75aa6482a1ffc80cd17d300
aliM494 <88322172+aliM494@users.noreply.github.com>
2021-10-24 16:47:50 +0330

    Added light/Darck mode to app

--- src/Timer.js
+++ src/Timer.js
@@ -23,13 +23,13 @@ class Timer extends React.Component {
         this.setState({
           second: this.state.second + 1
         });
-        if (this.state.second == 60) {
+        if (this.state.second === 60) {
           this.setState({
             second: 0,
             minute: this.state.minute + 1
           });
         }
-        if (this.state.minute == 60) {
+        if (this.state.minute === 60) {
           this.setState({
             minute: 0,
             hour: this.state.hour + 1
@@ -63,6 +63,13 @@ class Timer extends React.Component {
         <h2 className='timer'>
           {`${h > 9 ? h : "0" + h} : ${m > 9 ? m : "0" + m} : ${s > 9 ? s : "0" + s}`}
         </h2>
+        <span className='button_box action_button start' onClick={this.props.handleIsLight} 
+        style={
+          {
+            background : this.props.isLight ? "black" : "white",
+            color:  this.props.isLight ? "white" : "black"
+          }
+        }>{this.props.isLight ? "Dark" : "Light"}</span>
         <span className='button_box action_button reset' onClick={this.reset}>reset</span>
         <span className='button_box action_button stop' onClick={this.stop}>stop</span>
         <span className='button_box action_button start' onClick={this.start}>start</span>
